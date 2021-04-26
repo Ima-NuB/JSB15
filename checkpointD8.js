66a27b4522e321c2f4f26248cbc8f86b762a4316
Ima-NuB <80764308+Ima-NuB@users.noreply.github.com>
2021-04-26 19:08:56 +0700

    Add files via upload

--- checkpointD8.js
+++ checkpointD8.js
@@ -1,7 +1,9 @@
 function logIn(){
-    if(document.getElementById('input')=='example@gmail.com'){
-        if(document.getElementById('password')=='123'){
-            document.body.style="display='none'"
+    if(document.getElementById('input').value=='example@gmail.com'){
+        if(document.getElementById('password').value=='123'){
+            document.getElementById('loginSection').style="display:none"
+            document.getElementById('h1').innerHTML='LOGGED IN'
+            // document.getElementById('h1').style='display:block'
         }
         else{
             document.getElementById('insertPW').innerHTML='Invalid password'
