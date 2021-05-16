ceec833a7ee4bafcacf1f5b1853ac5f8d405ec0f
Ima-NuB <80764308+Ima-NuB@users.noreply.github.com>
2021-05-16 13:09:01 +0700

    Add files via upload

--- random.js
+++ random.js
@@ -2,5 +2,13 @@ function hit(){
     var array = ['red', 'blue', 'green', 'yellow', 'aqua', 'pink']
 
     var i = Math.floor(Math.random()*6)
+
     document.body.style.backgroundColor=array[i]
+    timer = setTimeout(
+        hit, 0
+    )
+}
+
+function ảoThậtĐấy(){
+    clearTimeout(timer)
 }
