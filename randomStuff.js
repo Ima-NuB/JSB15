c666f9e62969100b0ec6445cf4c4cd679a437f8d
Ima-NuB <80764308+Ima-NuB@users.noreply.github.com>
2021-05-22 22:34:16 +0700

    Add files via upload

---
+++ randomStuff.js
@@ -0,0 +1,32 @@
+function change() {
+    var color = document.getElementById('color').value
+    document.body.style.backgroundColor=color
+}
+
+function cHaNge(){
+    var color = document.getElementById('red').value
+    document.body.style.backgroundColor=color
+}
+
+function cHaNge1(){
+    var color = document.getElementById('yellow').value
+    document.body.style.backgroundColor=color
+}
+
+function cHaNge2(){
+    var color = document.getElementById('blue').value
+    document.body.style.backgroundColor=color
+}
+
+function cHaNge3(){
+    var color = document.getElementById('green').value
+    document.body.style.backgroundColor=color
+}
+
+function reset(){
+    document.body.style.backgroundColor='white'
+}
+
+function random(){
+    
+}
